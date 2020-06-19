46f6212e54c13280af8b08ec8cf0f3212d9d503f
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-06-19 08:25:27 -1000

    implementing health and damage

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,10 +6,18 @@ public class Projectile : MonoBehaviour
 {
 
     [SerializeField] float speed = 1f;
+    [SerializeField] float damage = 50;
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+        //reduce health
+        health.DealDamage(damage);
     }
 }
