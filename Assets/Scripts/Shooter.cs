98a95892d3279703380901aa9c8660e41c6f0f6f
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-07-02 10:14:13 -1000

    detect attacker in lane

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,6 +6,53 @@ public class Shooter : MonoBehaviour
 {
 
     [SerializeField] GameObject projectile, gun;
+    AttackerSpawner myLaneSpawner;
+
+    private void Start()
+    {
+        SetLaneSpawner();    
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("shoot pew pew pew");
+            //change animation state to shooting
+        }
+        else
+        {
+            Debug.Log("Sit and wait...");
+            //change animation state to idel
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+        foreach(AttackerSpawner spawner in spawners)
+        {
+            bool IsCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        //if my lane spawner child count is less than or equal to 0 return false
+        if(myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 
     public void Fire()
     {
