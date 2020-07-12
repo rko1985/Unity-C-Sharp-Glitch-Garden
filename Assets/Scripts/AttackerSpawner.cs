476060be541c2ea35b7316adb70b267fcdf8dc4f
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-07-11 17:51:39 -1000

    array for spawning attackers

--- Assets/Scripts/AttackerSpawner.cs
+++ Assets/Scripts/AttackerSpawner.cs
@@ -7,7 +7,7 @@ public class AttackerSpawner : MonoBehaviour
 
     [SerializeField] float minSpawnDelay = 1f;
     [SerializeField] float maxSpawnDelay = 5f;
-    [SerializeField] Attacker attackerPrefab;
+    [SerializeField] Attacker[] attackerPrefabArray;
 
     bool spawn = true;
 
@@ -23,16 +23,16 @@ public class AttackerSpawner : MonoBehaviour
 
     private void SpawnAttacker()
     {
-        Attacker newAttacker = Instantiate
-            (attackerPrefab, transform.position, transform.rotation) 
-            as Attacker;
-        newAttacker.transform.parent = transform;
+        var attackerIndex = Random.Range(0, attackerPrefabArray.Length);
+        Spawn(attackerPrefabArray[attackerIndex]);
 
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void Spawn(Attacker myAttacker)
     {
-        
+        Attacker newAttacker = Instantiate
+            (myAttacker, transform.position, transform.rotation)
+            as Attacker;
+        newAttacker.transform.parent = transform;
     }
 }
