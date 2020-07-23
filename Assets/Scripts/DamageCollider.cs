c0b8beaced002e2fce944ff876f82f4906824a1b
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-07-23 07:38:09 -1000

    fixing win condition bug

--- Assets/Scripts/DamageCollider.cs
+++ Assets/Scripts/DamageCollider.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class DamageCollider : MonoBehaviour
 {
 
-    private void OnTriggerEnter2D()
+    private void OnTriggerEnter2D(Collider2D otherCollider)
     {
         FindObjectOfType<LivesDisplay>().TakeLife();
+        Destroy(otherCollider.gameObject);
     }
 
 }
